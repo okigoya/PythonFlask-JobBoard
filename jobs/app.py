312924dbf3f0b48b2d286c6fa2e75a79af31b1d7
Lisa <lisasimmons@outlook.com>
2019-02-10 19:30:09 -0500

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -2,13 +2,14 @@ import sqlite3
 from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
+
 app = Flask(__name__)
 
 
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        connection = g._connection = sqlite.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
